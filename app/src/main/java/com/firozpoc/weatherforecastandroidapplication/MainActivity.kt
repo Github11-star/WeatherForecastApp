3ed991f98fdd6f8a74c5d4e4fd1b9f1d2a65acd2
Firoz Shaikh <firoz.md@atos.net>
2022-08-22 15:47:59 +0530

    location services added

--- app/src/main/java/com/firozpoc/weatherforecastandroidapplication/MainActivity.kt
+++ app/src/main/java/com/firozpoc/weatherforecastandroidapplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.firozpoc.weatherforecastandroidapplication
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.firozpoc.weatherforecastandroidapplication.databinding.ActivityMainBinding
 import com.firozpoc.weatherforecastandroidapplication.model.Values
 import com.firozpoc.weatherforecastandroidapplication.viewmodel.WeatherViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +26,8 @@ import java.nio.charset.Charset
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     private lateinit var binding: ActivityMainBinding
     private val viewModel: WeatherViewModel by viewModels()
 
@@ -23,6 +35,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getCurrentLocation()
+
 
         viewModel.weatherResponse.observe(this) { values ->
 
@@ -66,5 +83,87 @@ class MainActivity : AppCompatActivity() {
             return null
         }
         return json
+    }
+
+    // location service code here
+    // getting the current location using latitude and longitude
+    private fun getCurrentLocation(){
+        if (checkPermission()){
+            if (isLocationEnabled()){
+
+                // final latitude and longitude code here
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task->
+                    val location : Location?=task.result
+                    if (location == null){
+                        Toast.makeText(this, "Null Recieved", Toast.LENGTH_LONG).show()
+                    }
+                    else{
+                        Toast.makeText(this, "Get Success", Toast.LENGTH_LONG).show()
+                        tvLatitude.text = ""+location.latitude
+                        tvLongitude.text = ""+location.longitude
+                    }
+                }
+
+            }else{
+                // setting open here
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+        else{
+            // request permission here
+            requestPermission()
+        }
+    }
+
+    private fun isLocationEnabled() : Boolean {
+        val locationManager : LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    companion object{
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+
+    private fun checkPermission() : Boolean {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+
+            return true
+        }
+
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_REQUEST_ACCESS_LOCATION){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(applicationContext, "Granted", Toast.LENGTH_LONG).show()
+                getCurrentLocation()
+            }
+            else{
+                Toast.makeText(applicationContext, "Denied", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
