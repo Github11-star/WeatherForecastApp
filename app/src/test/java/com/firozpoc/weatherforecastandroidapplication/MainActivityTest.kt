fb653729e345e1099313e179f0e49d72611d68c2
Firoz Shaikh <firoz.md@atos.net>
2022-08-24 13:12:57 +0530

    added google truth library and mokito library

---
+++ app/src/test/java/com/firozpoc/weatherforecastandroidapplication/MainActivityTest.kt
@@ -0,0 +1,27 @@
+package com.firozpoc.weatherforecastandroidapplication
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class MainActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+
+    @Test
+    fun getCurrentLocationTest(){
+
+    }
+}
